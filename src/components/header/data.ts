61ce84351ddb639746c0c56aa2fee19709517ca5
Parth Sonera <107410994+psonera@users.noreply.github.com>
2023-07-20 12:34:55 +0530

    Update data.ts

--- src/components/header/data.ts
+++ src/components/header/data.ts
@@ -3,16 +3,16 @@ export const HeaderData = [
         label: 'About',
         link: '#about',
     },
-    {
-        label: 'Speakers',
-        link: '#speakers',
-    },
-    {
-        label: 'Our Team',
-        link: '#teams',
-    },
+    // {
+    //     label: 'Speakers',
+    //     link: '#speakers',
+    // },
+    // {
+    //     label: 'Our Team',
+    //     link: '#teams',
+    // },
     {
         label: 'Become volunteer',
-        link: '#',
+        link: 'https://docs.google.com/forms/d/e/1FAIpQLScGFblFW2xATLg27e4mVJAr_b4XH8rlDHEzdGrgjBe82kPUfA/viewform?usp=sf_link',
     },
 ];
